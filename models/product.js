229dd4a625bb322a4459940bd2acaea2f0a21e1b
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-10-06 23:23:00 -0300

    Module 9 - Dynamic Routes & Advanced Models

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const pathUtil = require('../util/path');
+const Cart = require('./cart');
 const p = path.join(pathUtil, 'data', 'products.json');
 
 
@@ -41,6 +42,20 @@ module.exports =  class Product {
             }
 
         });
+    }
+
+    static deleteById(id) {
+    
+        getProductsFromFile(products => {
+            const productIndex = products.findIndex(prod => prod.id == id);
+            let updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                if(!err) {
+                    Cart.deleteProduct(id, products[productIndex].price);
+
+                }
+            });
+        })
     }
 
     static fetchAll(cb) {
