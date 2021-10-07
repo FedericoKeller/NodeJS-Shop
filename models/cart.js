fcc61b7b698e56c1669807ad9cebc88bf238f10e
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-10-06 23:23:00 -0300

    Module 9 - Dynamic Routes & Advanced Models

--- models/cart.js
+++ models/cart.js
@@ -23,6 +23,7 @@ module.exports = class Cart {
             cart.products = [...cart.products];
             cart.products[existingProductIndex] = updatedProduct;
         } else {
+        //Add new product / increase quantity
             updatedProduct = {
                 id: id,
                 qty: 1,
@@ -38,7 +39,40 @@ module.exports = class Cart {
         });
 
         })
-        //Add new product / increase quantity
+    }
+
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+           
+            if (err) {
+                return;
+            }
+
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id === id);
+            if(!product) {
+                return;
+            }
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
+                console.log(err);
+            });
+        })
+    }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) {
+                cb(null);
+            } else {
+                cb(cart);
+            }
+        });
     }
 
 
