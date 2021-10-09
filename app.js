7aaab898dbb044fff1a78fdbdd79b82947801faf
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-10-09 15:06:02 -0300

    Module 10 - SQL Introduction

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const path = require('path');
-const express = require('express');
-const nunjucks = require('nunjucks');
-const setUpNunjucks = require('./helpers/nunjuck_helpers');
-const errorController = require('./controllers/error');
+const path = require("path");
+
+const express = require("express");
+
+const nunjucks = require("nunjucks");
+const setUpNunjucks = require("./helpers/nunjuck_helpers");
+
+const errorController = require("./controllers/error");
+
+const db = require("./util/database");
+
 const app = express();
 
-app.engine('njk', nunjucks.render);
-app.set('view engine', 'njk');
+app.engine("njk", nunjucks.render);
+app.set("view engine", "njk");
 
 setUpNunjucks(app);
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-
-app.use(express.urlencoded({extended: false})); 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/admin', adminRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- models/product.js
+++ models/product.js
@@ -1,18 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const pathUtil = require('../util/path');
+
+const db = require ('../util/database');
 const Cart = require('./cart');
-const p = path.join(pathUtil, 'data', 'products.json');
-
-
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if(err) { 
-           return cb([]);
-        }
-        cb(JSON.parse(fileContent));
-    })
-}
 
 module.exports =  class Product {
     constructor(id, title, imageUrl, description, price) {
@@ -24,49 +12,22 @@ module.exports =  class Product {
     }
 
     save() {
-        getProductsFromFile(products => {
-            if(this.id) {
-                const existingProductIndex =products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err);
-                });
-
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);    
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log(err);
-                });
-            }
-
-        });
+     return db.execute(
+         "INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)", 
+         [this.title, this.price, this.description, this.imageUrl]
+         )    
     }
 
     static deleteById(id) {
     
-        getProductsFromFile(products => {
-            const productIndex = products.findIndex(prod => prod.id == id);
-            let updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                if(!err) {
-                    Cart.deleteProduct(id, products[productIndex].price);
-
-                }
-            });
-        })
+      
     }
 
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
+    static fetchAll() {
+     return db.execute("SELECT * FROM products");
     }
 
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(product => product.id == id);
-            cb(product);
-        
-        })
+    static findById(id) {
+        return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
     }
 }
